64323fb9157c8be1e518a37cd8937b16804bc2da
show <show75@ezweb.ne.jp>
2020-03-31 22:32:20 +0900

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClpCurrentUser from './molecules/ClpCurrentUser';
+import '../assets/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ClpCurrentUser></ClpCurrentUser>
+    </div>
+  );
+}
+
+export default App;
