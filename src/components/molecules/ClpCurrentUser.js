7f22cf2fee3ce8f71a61557a2b09dc3ce05223b3
show <show75@ezweb.ne.jp>
2020-04-01 11:53:35 +0900

    #1 ローカルストレージにユーザを格納

--- src/components/molecules/ClpCurrentUser.js
+++ src/components/molecules/ClpCurrentUser.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ClpIcon from '../atoms/ClpIcon'
+import ClpIcon from "../atoms/ClpIcon";
 
 class ClpCurrentUser extends React.Component {
   constructor(props) {
@@ -13,6 +13,12 @@ class ClpCurrentUser extends React.Component {
     return (
       <div>
         <h1>Hello React</h1>
+        <select name="name">
+          <option value="Nakata">Nakata</option>
+          <option value="Rachel">Rachel</option>
+          <option value="Kiyoshi">Kiyoshi</option>
+          <option value="Takada">Takada</option>
+        </select>
         <ClpIcon uid={1}></ClpIcon>
       </div>
     );
