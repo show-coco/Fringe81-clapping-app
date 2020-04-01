dd803380bbe2f9f5e938eb38a693b6acde4f196b
show <show75@ezweb.ne.jp>
2020-04-01 11:58:02 +0900

    #1 動的にユーザを変更

--- src/components/molecules/ClpCurrentUser.js
+++ src/components/molecules/ClpCurrentUser.js
@@ -1,25 +1,45 @@
 import React from "react";
 import ClpIcon from "../atoms/ClpIcon";
+/* eslint no-unused-expressions: "off" */
 
 class ClpCurrentUser extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.users = props.users;
     this.state = {
-      canClapNum: null,
-      clappedNum: null
+      id:  this.users[0].id,
+      name: this.users[0].name
     };
   }
+
+  handleChange(event) {
+    this.setState({
+        id: event.target.value,
+        name: this.users[event.target.value].name,
+    });
+  }
+
   render() {
+    const users = this.props.users;
+    const options = users.map(user => {
+      return (
+        <option value={user.id} key={user.id}>
+          {user.name}
+        </option>
+      );
+    });
+
     return (
       <div>
         <h1>Hello React</h1>
-        <select name="name">
-          <option value="Nakata">Nakata</option>
-          <option value="Rachel">Rachel</option>
-          <option value="Kiyoshi">Kiyoshi</option>
-          <option value="Takada">Takada</option>
+        <select name="name" onChange={this.handleChange}>
+          {options}
         </select>
-        <ClpIcon uid={1}></ClpIcon>
+        <p>{this.state.name}</p>
+        <p>{this.users[this.state.id].canClapNum}</p>
+        <p>{this.users[this.state.id].clappedNum}</p>
+        <ClpIcon uid={this.state.id}></ClpIcon>
       </div>
     );
   }
