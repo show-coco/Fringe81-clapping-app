251a4a7c93ced679d25b79cbd4449d1740155f3c
show <show75@ezweb.ne.jp>
2020-04-01 11:54:22 +0900

    #1 Warning回避

--- src/components/atoms/ClpIcon.js
+++ src/components/atoms/ClpIcon.js
@@ -3,7 +3,7 @@ import React from "react";
 function ClpIcon(props) {
   return (
     <div>
-      <img src={require(`../../assets/images/icon${props.uid}.png`)} />
+      <img src={require(`../../assets/images/icon${props.uid}.png`)} alt="icon" />
     </div>
   );
 }
